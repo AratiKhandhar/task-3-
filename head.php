5b55b2b4216d3ea2ea47e08e0dcffe9f2b7af54f
AratiKhandhar <156210732+AratiKhandhar@users.noreply.github.com>
2024-04-19 17:00:40 +0530

    Add files via upload

---
+++ head.php
@@ -0,0 +1,5 @@
+<?php
+header(" Refresh:2 https://www.google.com/");
+
+echo "It will redircet in two seconds if not"
+?>
---
+++ script_29.php
@@ -0,0 +1,4 @@
+<?php
+$a=array("a"=>"Shreya","b"=>"Dharmi","c"=>"arati");
+print_r(array_reverse($a));
+?>
